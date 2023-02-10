67a19608df67d78c848e8d4661b95c8e9f80224d
Ion Stici <ionsticidev@gmail.com>
2023-02-10 18:20:01 +0200

    Implement modal functionality

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -1,9 +1,136 @@
 const main = document.querySelector('.main');
 const modalLayout = document.querySelector('.modal__layout');
-const modal = document.querySelector('.modal');
+const modalWindow = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.modal__close-icon');
 
+const subBoxBambo = document.querySelector('.modal__box__active-box--bamboo');
+const subBoxBlack = document.querySelector('.modal__box__active-box--black');
+
+const activeBoxClass = 'modal__box--active';
+const modalBoxes = document.querySelectorAll('.modal__box');
+
+const allSubBoxes = document.querySelectorAll('.modal__box__active-box');
+
+// BUTTONS
 const btnBack = document.querySelector('.btn-back');
+const btnBamboo = document.querySelector('.btn-select-bamboo');
+const btnBlack = document.querySelector('.btn-select-black');
 
-btnBack.addEventListener('click', function () {
+// MARKUP
+const markup = `
+<p class="modal__box__active-box__text">Enter tour pledge</p>
+<div class="modal__box__active-box__wrapper">
+    <label class="modal__box__active-box__label">$</label>
+    <input class="modal__box__active-box__input" type="number">
+    <button class="btn">Continue</button>
+</div>
+`;
+
+// OPEN MODAL
+btnBack.addEventListener('click', () => main.classList.add('open-modal'));
+
+// CLOSE MODAL
+
+modalLayout.addEventListener('click', () => {
+    subBoxBlack.innerHTML = '';
+    subBoxBambo.innerHTML = '';
+    main.classList.remove('open-modal');
+
+    modalBoxes.forEach(box => box.classList.remove(activeBoxClass));
+});
+
+modalCloseBtn.addEventListener('click', () => {
+    subBoxBlack.innerHTML = '';
+    subBoxBambo.innerHTML = '';
+    main.classList.remove('open-modal');
+
+    modalBoxes.forEach(box => box.classList.remove(activeBoxClass));
+});
+
+// OPEN MODAL BY CATEGORY
+
+btnBamboo.addEventListener('click', function () {
     main.classList.add('open-modal');
+    location.href = '#top';
+    setTimeout(() => (location.href = '#'), 50);
+
+    subBoxBambo.innerHTML = '';
+    subBoxBambo.insertAdjacentHTML('afterbegin', markup);
+    subBoxBambo.closest('.modal__box').classList.add(activeBoxClass);
+    document.querySelector('.modal__box__active-box__input').value = 25;
+});
+
+btnBlack.addEventListener('click', function () {
+    main.classList.add('open-modal');
+    location.href = '#top';
+    setTimeout(() => (location.href = '#'), 50);
+
+    subBoxBlack.innerHTML = '';
+    subBoxBlack.insertAdjacentHTML('afterbegin', markup);
+    subBoxBlack.closest('.modal__box').classList.add(activeBoxClass);
+    document.querySelector('.modal__box__active-box__input').value = 75;
+});
+
+// MODAL BOXES
+modalBoxes.forEach(box => {
+    box.addEventListener('click', function (e) {
+        if (
+            !e.target.classList.contains('modal__box__active-box') &&
+            !e.target.classList.contains('modal__box__active-box__text') &&
+            !e.target.classList.contains('modal__box__active-box__wrapper') &&
+            !e.target.classList.contains('modal__box__active-box__input') &&
+            !e.target.classList.contains('btn')
+        ) {
+            //
+            if (
+                !box.classList.contains('modal__box--inactive') &&
+                !box.classList.contains(activeBoxClass)
+            ) {
+                box.classList.add(activeBoxClass);
+
+                const subBox = box.querySelector('.modal__box__active-box');
+
+                if (
+                    subBox &&
+                    subBox.classList.contains('modal__box__active-box--bamboo')
+                ) {
+                    subBoxBambo.innerHTML = '';
+                    subBoxBambo.insertAdjacentHTML('afterbegin', markup);
+                    subBox.querySelector(
+                        '.modal__box__active-box__input'
+                    ).value = 25;
+                }
+
+                if (
+                    subBox &&
+                    subBox.classList.contains('modal__box__active-box--black')
+                ) {
+                    subBoxBlack.innerHTML = '';
+                    subBoxBlack.insertAdjacentHTML('afterbegin', markup);
+                    subBox.querySelector(
+                        '.modal__box__active-box__input'
+                    ).value = 75;
+                }
+            } else if (box.classList.contains(activeBoxClass)) {
+                box.classList.remove(activeBoxClass);
+
+                const subBox = box.querySelector('.modal__box__active-box');
+
+                if (
+                    subBox &&
+                    subBox.classList.contains('modal__box__active-box--bamboo')
+                ) {
+                    subBoxBambo.innerHTML = '';
+                }
+
+                if (
+                    subBox &&
+                    subBox.classList.contains('modal__box__active-box--black')
+                ) {
+                    subBoxBlack.innerHTML = '';
+                }
+            }
+            //
+        }
+    });
 });
